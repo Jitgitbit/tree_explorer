fcc5f5e0c0afff559b8aec1e233b5be327e8b037
Jitgitbit <rowowolf@gmail.com>
2020-02-11 12:06:14 +0100

    renderin popTrees

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import Tree from './components/Tree';
+// import Tree from './components/Tree';
+import PopularTrees from './components/PopularTrees';
 
 const App = () => {
   return (
     <div className="App">
-      <Tree name={`White birch`} scientificName={`Betula pendula`}/>
+      {/* <Tree name={`White birch`} scientificName={`Betula pendula`}/>
       <Tree name={`Weeping willow`} scientificName={`Salix sepulcralis`}/>
-      <Tree name={`London planetree`} scientificName={`Platanus hybryda`}/>
-      
+      <Tree name={`London planetree`} scientificName={`Platanus hybryda`}/> */}
+      <PopularTrees/>
     </div>
   );
 }
--- src/components/PopularTrees.tsx
+++ src/components/PopularTrees.tsx
@@ -29,6 +29,7 @@ class PopularTrees extends React.Component<Props, State> {
         />
       ))}
     </div>
+    
     )
   }
 }
