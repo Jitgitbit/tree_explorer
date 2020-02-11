edf95e8bf2ffb72d2b94d10e6defd9514cc737d9
Jitgitbit <rowowolf@gmail.com>
2020-02-11 14:16:40 +0100

    dumdum button

--- src/components/PopularTrees.tsx
+++ src/components/PopularTrees.tsx
@@ -7,29 +7,87 @@ type State = {
   trees: Array<{
     name: string;
     scientificName: string;
+    commonName: string;
+    numLikes: number;
   }>;
+
+  commonName: string
+  scientificName: string
+  numLikes: number
 };
 class PopularTrees extends React.Component<Props, State> {
   state: State = {
     trees: [
-      { name: "White birch", scientificName: "Betula pendula" },
-      { name: "Weeping willow", scientificName: "Salix sepulcralis" },
-      { name: "London planetree", scientificName: "Platanus hybryda" }
-    ]
+      { name: "White birch", scientificName: "Betula pendula", commonName: `something`, numLikes:3 },
+      { name: "Weeping willow", scientificName: "Salix sepulcralis", commonName: `something`, numLikes:76576565744 },
+      { name: "London planetree", scientificName: "Platanus hybryda", commonName: `something`, numLikes:2.3 }
+    ],
+    commonName: '',
+    scientificName: '',
+    numLikes: 0
   };
   
+  handleLike = () => {
+    console.log("hi");
+  };
+
+  onLike = () => {
+    console.log(`hi`);
+  }
+
+  addTree =()=> {
+    console.log(`adding tree log`)
+  }
+
   render() {
     return (
-    <div>
-      {this.state.trees.map((tree, index) => (
-        <Tree
-          key={index}
-          name={tree.name}
-          scientificName={tree.scientificName}
-        />
-      ))}
+    <div> 
+      <div>
+        {this.state.trees.map((tree, index) => (
+          <Tree
+            key={index}
+            name={tree.name}
+            scientificName={tree.scientificName}
+            commonName={tree.commonName}
+            numLikes={tree.numLikes}
+            onLike={this.onLike}
+          />
+        ))}
+      </div>
+      <div>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            this.addTree();
+          }}
+          style={{ display: "flex" }}
+        >
+          <p style={{ margin: ".25rem" }}>
+            <label>
+              Common name:{" "}
+              <input
+                type="text"
+                value={this.state.commonName}
+                onChange={e => this.setState({ commonName: e.target.value })}
+              />
+            </label>
+          </p>
+          <p style={{ margin: ".25rem" }}>
+            <label>
+              Scientific name:{" "}
+              <input
+                type="text"
+                value={this.state.scientificName}
+                onChange={e => this.setState({ scientificName: e.target.value })}
+              />
+            </label>
+          </p>
+          <p style={{ margin: ".25rem" }}>
+            <button type="submit">Add!</button>
+          </p>
+        </form>
+      </div>
     </div>
-    
     )
   }
 }
--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -1,12 +1,25 @@
 // src/components/Tree.tsx
 import React from "react";
+
 type Props = {
   name: string;
   scientificName: string;
+  commonName: string;
+  numLikes: number;
+  onLike: VoidFunction;
 };
 class Tree extends React.Component<Props> {
   render() {
-    return <p>Name: {this.props.name}, Scientific Name: {this.props.scientificName}</p>;
+    return (
+    <div>
+      <p>Name: {this.props.name}, 
+      Scientific Name: {this.props.scientificName}, 
+      Common Name: {this.props.commonName}, 
+      Likes: {this.props.numLikes}</p>
+
+      <button onClick={this.props.onLike}>Like</button>
+    </div>
+    )
   }
 }
 export default Tree;
