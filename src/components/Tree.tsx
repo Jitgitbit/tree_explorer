df8e7f31e8d073818a1ee644b0f5b4e2141eadb6
Jitgitbit <rowowolf@gmail.com>
2020-02-11 11:07:51 +0100

    Tree added

---
+++ src/components/Tree.tsx
@@ -0,0 +1,11 @@
+// src/components/Tree.tsx
+import React from "react";
+type Props = {
+  name: string;
+};
+class Tree extends React.Component<Props> {
+  render() {
+    return <p>Name: {this.props.name}</p>;
+  }
+}
+export default Tree;
